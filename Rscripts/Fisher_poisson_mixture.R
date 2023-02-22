99cf37862bf1dfde2d393f7edd42bda09a9f7f76
madelattre <maud.delattre@inrae.fr>
2023-02-22 14:05:16 +0100

    Fix formula errors in the computation of Isco and Iobs

--- Rscripts/Fisher_poisson_mixture.R
+++ Rscripts/Fisher_poisson_mixture.R
@@ -75,11 +75,11 @@ pm_fisher_estimation <- function(y, lambda, alpha) {
   denom <- denom + exp(-lambda[K])*lambda[K]^y*(1-sum(alpha))
   
   for (k in 1:(K-1)){
-    deriv1ind[k,]   <- exp(-lambda[k])*lambda[k]^y*alpha[k]/denom * (y/lambda[k]-1)
-    deriv1ind[K+k,] <- exp(-lambda[k])*lambda[k]^y*alpha[k]/denom/alpha[k] + 
-      exp(-lambda[K])*lambda[K]^y*(1-sum(alpha))/denom*(-1/(1-sum(alpha)))
+    deriv1ind[k,]   <- exp(-lambda[k])*lambda[k]^y*alpha[k]/denom*(y/lambda[k]-1)
+    deriv1ind[K+k,] <- exp(-lambda[k])*lambda[k]^y/denom - 
+      exp(-lambda[K])*lambda[K]^y/denom
   }
-  deriv1ind[K,] <- exp(-lambda[K])*lambda[K]^y*(1-sum(alpha))/denom * (y/lambda[K]-1)
+  deriv1ind[K,] <- exp(-lambda[K])*lambda[K]^y*(1-sum(alpha))/denom*(y/lambda[K]-1)
   
   
   ## computation of conditional expectation of the second derivatives of the 
@@ -87,19 +87,19 @@ pm_fisher_estimation <- function(y, lambda, alpha) {
   
   
   for (k in 1:(K-1)){
-    deriv2[k,k]     <- sum(exp(-lambda[k])*lambda[k]^y*alpha[k]/denom * (-y/lambda[k]^2))
-    deriv2[K+k,K+k] <- sum(-exp(-lambda[k])*lambda[k]^y*alpha[k]/denom/alpha[k]^2 
-                           + exp(-lambda[K])*lambda[K]^y*(1-sum(alpha))/denom*(-1/(1-sum(alpha))^2))
+    deriv2[k,k]     <- sum(exp(-lambda[k])*lambda[k]^y*alpha[k]/denom*(-y/lambda[k]^2))
+    deriv2[K+k,K+k] <- sum(-exp(-lambda[k])*lambda[k]^y/denom/alpha[k] -
+                           exp(-lambda[K])*lambda[K]^y/denom*(1/(1-sum(alpha))))
   }
   
   for (k in 1:(K-2)){
     for (l in (k+1):(K-1)){
-      deriv2[K+k,K+l] <- sum(exp(-lambda[K])*lambda[K]^y*(1-sum(alpha))/denom*(-1/(1-sum(alpha))^2))
+      deriv2[K+k,K+l] <- - sum(exp(-lambda[K])*lambda[K]^y/denom*(1/(1-sum(alpha))))
       deriv2[K+l,K+k] <- deriv2[K+k,K+l] 
     }
   }
   
-  deriv2[K,K]<-sum(exp(-lambda[K])*lambda[K]^y*(1-sum(alpha))/denom * (-y/lambda[K]^2))
+  deriv2[K,K]<-sum(exp(-lambda[K])*lambda[K]^y*(1-sum(alpha))/denom*(-y/lambda[K]^2))
   
   ## computation of the conditional covariance matrix of the first derivatives of 
   ## the complete data log-likelihood
@@ -107,26 +107,26 @@ pm_fisher_estimation <- function(y, lambda, alpha) {
   
   for (k in 1:(K-2)){
     covderiv[k,k] <- sum(exp(-lambda[k])*lambda[k]^y*alpha[k]/denom*(-1+y/lambda[k])^2) 
-    covderiv[k+K,k+K] <- sum(exp(-lambda[k])*lambda[k]^y*alpha[k]/denom/alpha[k]^2
-                             +exp(-lambda[K])*lambda[K]^y*(1-sum(alpha))/denom/(1-sum(alpha))^2) 
+    covderiv[k+K,k+K] <- sum(exp(-lambda[k])*lambda[k]^y/denom/alpha[k] +
+                             exp(-lambda[K])*lambda[K]^y/denom/(1-sum(alpha))) 
     for (l in (k+1):(K-1)){
-      covderiv[k+K,l+K] <- sum(exp(-lambda[K])*lambda[K]^y*(1-sum(alpha))/denom/(1-sum(alpha))^2) 
+      covderiv[k+K,l+K] <- sum(exp(-lambda[K])*lambda[K]^y/denom/(1-sum(alpha))) 
       covderiv[l+K,k+K] <- covderiv[k+K,l+K]
     } 
-    covderiv[k,K+k] <- sum(exp(-lambda[k])*lambda[k]^y*alpha[k]/denom/alpha[k]*(-1+y/lambda[k])) 
+    covderiv[k,K+k] <- sum(exp(-lambda[k])*lambda[k]^y/denom*(-1+y/lambda[k])) 
     covderiv[k+K,k] <- covderiv[k,K+k]
     
-    covderiv[K,K+k] <- sum(exp(-lambda[K])*lambda[K]^y*(1-sum(alpha))/denom*(-1+y/lambda[K])*(-1)/(1-sum(alpha))) 
+    covderiv[K,K+k] <- sum(exp(-lambda[K])*lambda[K]^y/denom*(-1+y/lambda[K])*(-1)) 
     covderiv[K+k,K] <- covderiv[K,K+k]
   }
   
   covderiv[K-1,K-1] <- sum(exp(-lambda[K-1])*lambda[K-1]^y*alpha[K-1]/denom*(-1+y/lambda[K-1])^2) 
-  covderiv[2*K-1,2*K-1] <- sum(exp(-lambda[K-1])*lambda[K-1]^y*alpha[K-1]/denom/alpha[K-1]^2+
-                                 exp(-lambda[K])*lambda[K]^y*(1-sum(alpha))/denom/(1-sum(alpha))^2) 
-  covderiv[K-1,2*K-1] <- sum(exp(-lambda[K-1])*lambda[K-1]^y*alpha[K-1]/denom/alpha[K-1]*(-1+y/lambda[K-1])) 
+  covderiv[2*K-1,2*K-1] <- sum(exp(-lambda[K-1])*lambda[K-1]^y/denom/alpha[K-1]+
+                                 exp(-lambda[K])*lambda[K]^y/denom/(1-sum(alpha))) 
+  covderiv[K-1,2*K-1] <- sum(exp(-lambda[K-1])*lambda[K-1]^y/denom*(-1+y/lambda[K-1])) 
   covderiv[2*K-1,K-1] <- covderiv[K-1,2*K-1]
   
-  covderiv[K,2*K-1] <- sum(exp(-lambda[K])*lambda[K]^y*(1-sum(alpha))/denom*(-1+y/lambda[K])*(-1)/(1-sum(alpha))) 
+  covderiv[K,2*K-1] <- sum(exp(-lambda[K])*lambda[K]^y/denom*(-1+y/lambda[K])*(-1)/(1-sum(alpha))) 
   covderiv[2*K-1,K] <- covderiv[K,2*K-1]
   
   covderiv[K,K] <- sum(exp(-lambda[K])*lambda[K]^y*(1-sum(alpha))/denom*(-1+y/lambda[K])^2) 
@@ -145,7 +145,7 @@ pm_fisher_estimation <- function(y, lambda, alpha) {
 ## Numerical experiment -------------------------------------------------------
 
 
-nbsim  <- 10#500        # number of replicates
+nbsim  <- 100 #500        # number of replicates
 nMC    <- 100000000  # sample size for Monte-Carlo estimation of the FIM
 alpha  <- c(0.3,0.5) # mixture weights of the first K-1 components 
 lambda <- c(2,5,9)   # parameter values of the K Poisson distribution of the mixture
@@ -156,7 +156,8 @@ theta.true <- matrix(c(lambda,alpha),ncol=1)
 ## MCMC estimation of the FIM
 
 fisher.mcmc <- pm_fisher_mcmc(lambda,alpha,nMC)
-
+inv.sqrt.fisher.mcmc <- solve(chol(fisher.mcmc))
+## Cholesky de fisher.mcmc à caluler une seule fois, en dehors de la boucle
 
 ## Computation of Isco and Iobs based on nbsim simulated samples
 
@@ -238,9 +239,9 @@ while (j <= nbsim){
     ## ... based on the true Fisher information matrix
     
     conf.inf.fisher.true <- theta.est -
-      1.96/sqrt(n)*solve(chol(fisher.mcmc))%*%unit.vect
+      1.96/sqrt(n)*inv.sqrt.fisher.mcmc%*%unit.vect
     conf.sup.fisher.true <- theta.est +
-      1.96/sqrt(n)*solve(chol(fisher.mcmc))%*%unit.vect
+      1.96/sqrt(n)*inv.sqrt.fisher.mcmc%*%unit.vect
     
     coverage.true.fisher <- coverage.true.fisher +
       (theta.true<=conf.sup.fisher.true)*(theta.true>=conf.inf.fisher.true)
