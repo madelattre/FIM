fe40cacb1dcc8118ed350453095042993d28cb4a
madelattre <maud.delattre@inrae.fr>
2023-03-08 15:11:30 +0100

    Correct formula for the computation of the coverage rates

--- Rscripts/Fisher_linear_mixed_model.R
+++ Rscripts/Fisher_linear_mixed_model.R
@@ -174,9 +174,9 @@ for (n in seq.n){
     ## ... based on the true Fisher information matrix
     
     conf.inf.fisher.true <- theta.est -
-      1.96/sqrt(n)*solve(chol(fisher))%*%unit.vect
+      1.96/sqrt(n)*sqrt(diag(solve(fisher)))
     conf.sup.fisher.true <- theta.est +
-      1.96/sqrt(n)*solve(chol(fisher))%*%unit.vect
+      1.96/sqrt(n)*sqrt(diag(solve(fisher)))
     
     coverage.true.fisher <- coverage.true.fisher +
       (theta.true<=conf.sup.fisher.true)*(theta.true>=conf.inf.fisher.true)
@@ -184,36 +184,36 @@ for (n in seq.n){
     
     ## ... based on Isco computed in the MLE value of the parameters
     conf.inf.isco.theta.est <- theta.est -
-      1.96/sqrt(n)*solve(chol(resIsco.theta.est[,,k]))%*%unit.vect
+      1.96/sqrt(n)*sqrt(diag(solve(resIsco.theta.est[,,k])))
     conf.sup.isco.theta.est <- theta.est +
-      1.96/sqrt(n)*solve(chol(resIsco.theta.est[,,k]))%*%unit.vect
+      1.96/sqrt(n)*sqrt(diag(solve(resIsco.theta.est[,,k])))
     
     coverage.isco.theta.est <- coverage.isco.theta.est +
       (theta.true<=conf.sup.isco.theta.est)*(theta.true>=conf.inf.isco.theta.est)
     
     ## ... based on Isco computed in the true parameter values
     conf.inf.isco.theta.true <- theta.est -
-      1.96/sqrt(n)*solve(chol(resIsco.theta.true[,,k]))%*%unit.vect
+      1.96/sqrt(n)*sqrt(diag(solve(resIsco.theta.true[,,k])))
     conf.sup.isco.theta.true <- theta.est +
-      1.96/sqrt(n)*solve(chol(resIsco.theta.true[,,k]))%*%unit.vect
+      1.96/sqrt(n)*sqrt(diag(solve(resIsco.theta.true[,,k])))
     
     coverage.isco.theta.true <- coverage.isco.theta.true +
       (theta.true<=conf.sup.isco.theta.true)*(theta.true>=conf.inf.isco.theta.true)
     
     ## ... based on Iobs computed in the MLE value of the parameters
     conf.inf.iobs.theta.est <- theta.est -
-      1.96/sqrt(n)*solve(chol(resIobs.theta.est[,,k]))%*%unit.vect
+      1.96/sqrt(n)*sqrt(diag(solve(resIobs.theta.est[,,k])))
     conf.sup.iobs.theta.est <- theta.est +
-      1.96/sqrt(n)*solve(chol(resIobs.theta.est[,,k]))%*%unit.vect
+      1.96/sqrt(n)*sqrt(diag(solve(resIobs.theta.est[,,k])))
     
     coverage.iobs.theta.est <- coverage.iobs.theta.est +
       (theta.true<=conf.sup.iobs.theta.est)*(theta.true>=conf.inf.iobs.theta.est)
     
     ## ... based on Iobs computed in the true parameter values
     conf.inf.iobs.theta.true <- theta.est -
-      1.96/sqrt(n)*solve(chol(resIobs.theta.true[,,k]))%*%unit.vect
+      1.96/sqrt(n)*sqrt(diag(solve(resIobs.theta.true[,,k])))
     conf.sup.iobs.theta.true <- theta.est +
-      1.96/sqrt(n)*solve(chol(resIobs.theta.true[,,k]))%*%unit.vect
+      1.96/sqrt(n)*sqrt(diag(solve(resIobs.theta.true[,,k])))
 
     coverage.iobs.theta.true <- coverage.iobs.theta.true +
       (theta.true<=conf.sup.iobs.theta.true)*(theta.true>=conf.inf.iobs.theta.true)
